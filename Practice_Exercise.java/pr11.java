e748a115c8e8b1605765b6997d134a91e2dc0a36
pateldev0606-maker <pateldev0606@gmail.com>
2026-04-12 22:10:09 +0530

    Create pr11.java

---
+++ Practice_Exercise.java/pr11.java
@@ -0,0 +1,46 @@
+package set;
+
+import java.util.Scanner;
+
+class College {
+
+    String collegeName;
+
+    College(String name) {
+        collegeName = name;
+    }
+
+    class Admission {
+        String studentName;
+        String course;
+
+        void input() {
+            Scanner sc = new Scanner(System.in);
+
+            System.out.print("Enter Student Name: ");
+            studentName = sc.nextLine();
+
+            System.out.print("Enter Course: ");
+            course = sc.nextLine();
+        }
+
+        void display() {
+            System.out.println("\n-- Admission Details --");
+            System.out.println("College Name: " + collegeName); 
+            System.out.println("Student Name: " + studentName);
+            System.out.println("Course: " + course);
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        College c = new College("GTU College");
+
+        College.Admission a = c.new Admission();
+
+        a.input();
+        a.display();
+    }
+}
