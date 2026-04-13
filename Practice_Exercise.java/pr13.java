2f8bafdffea708176ca16863339f7d3ceb266042
pateldev0606-maker <pateldev0606@gmail.com>
2026-04-13 23:10:47 +0530

    Create pr13.java

---
+++ Practice_Exercise.java/pr13.java
@@ -0,0 +1,37 @@
+package set;
+
+class Shape {
+    double d1, d2;
+
+    void getData(double d1, double d2) {
+        this.d1 = d1;
+        this.d2 = d2;
+    }
+}
+
+class Triangle extends Shape {
+
+    double calculateArea() {
+        return 0.5 * d1 * d2;
+    }
+}
+
+class Rectangle extends Shape {
+
+    double calculateArea() {
+        return d1 * d2;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Triangle t = new Triangle();
+        t.getData(20, 5);
+        System.out.println("Area of Triangle = " + t.calculateArea());
+
+        Rectangle r = new Rectangle();
+        r.getData(20, 5);
+        System.out.println("Area of Rectangle = " + r.calculateArea());
+    }
+}
